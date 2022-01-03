1119f682b436a5a0385b35d45d22dadf197a4342
Aexander Larichev <ZevanZor@yandex.ru>
2022-01-03 17:31:56 +0300

    fix: исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -3,34 +3,31 @@ const popupCloseButton = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
 
 const popupSave = document.querySelector(".popup__save");
-const popupName = document.querySelector(".popup__name");
-const popupInfo = document.querySelector(".popup__info");
+const popupName = document.querySelector(".popup__input_name");
+const popupInfo = document.querySelector(".popup__input_info");
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+const formProfile = document.querySelector(".popup__profile")
 
 
-function OpenButton() {
+function openButton() {
   popup.classList.add("popup_open");
   popupName.value = profileTitle.textContent;
-  popupInfo.value = profileSubtitle.textContent; 
-   
+  popupInfo.value = profileSubtitle.textContent;
 }
 
 function CloseButton() {
   popup.classList.remove("popup_open");
 }
 
-
-popupOpenButton.addEventListener("click", OpenButton);
-popupCloseButton.addEventListener("click", CloseButton);
-popupSave.addEventListener('click', CloseButton);
-
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitle.textContent = popupName.value;
-  profileSubtitle.textContent = popupInfo.value;  
+  profileSubtitle.textContent = popupInfo.value;
+  CloseButton()  
 }
 
-popupSave.addEventListener('click', formSubmitHandler);
-popupSave.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener ('submit', formSubmitHandler)
+
+popupOpenButton.addEventListener("click", openButton);
+popupCloseButton.addEventListener("click", CloseButton);
